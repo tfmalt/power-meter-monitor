c390257c08e84e1518c9a5dcc8372beef2c9a063
Thomas Malt <thomas@malt.no>
2014-06-01 22:19:42 +0200

    jslinted monitor

--- monitor.js
+++ monitor.js
@@ -9,19 +9,22 @@ var config = require('./config-private'),
     domain = require('domain').create();
 
 domain.on("error", function (err) {
-    logger.error("Got an error event stack trace:", err.message, err.stack); 
+    "use strict";
+    logger.error("Got an error event stack trace:", err.message, err.stack);
     process.exit(1);
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
+    "use strict";
     logger.info("Got SIGINT. Told to exit. so bye.");
     process.exit(0);
 });
 
 domain.run(function () {
+    "use strict";
     logger.remove(logger.transports.Console);
     logger.add(logger.transports.File, {
-        colorize:  true, 
+        colorize:  true,
         timestamp: true,
         filename: "/var/log/power-meter/monitor.log",
         maxsize: 100000000,
@@ -29,6 +32,7 @@ domain.run(function () {
         json: false
     });
 
+    console.log("power-meter-monitor version 0.4.6");
     meter.startMonitor(config.redis);
 
     logger.info("Power meter monitoring started in master script");
