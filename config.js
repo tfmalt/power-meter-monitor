3f4211bb8f630b95489e2eaec95f8ae31ec8b5d8
Thomas Malt <thomas@malt.no>
2015-04-12 13:13:24 +0200

    configuring instance with environment varialbles instead

--- config.js
+++ config.js
@@ -1,1 +1,21 @@
-config-private.js
+var pj = require('./package.json');
+
+exports.version = pj.version;
+
+if (process.env.POWER_ENV === "development") {
+    exports.logfile = "monitor.log";
+} else {
+    exports.logfile = "/var/log/power-meter/monitor.log";
+}
+
+exports.redis = {
+    host: "localhost",
+    port: 6379,
+    options: {
+        auth_pass: null
+    }
+};
+
+exports.serial =  {
+    dev: "/tmp/meteroutput"
+};
