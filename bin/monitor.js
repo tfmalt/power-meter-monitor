092d2e3ad002d8e7261b0db82a73a17fb332944b
Thomas Malt <thomas@malt.no>
2017-03-25 16:41:16 +0100

    adding statistics

--- bin/monitor.js
+++ bin/monitor.js
@@ -55,7 +55,7 @@ meter.on('reset_counter', (info) => {
 
   const zero = average(info.pulses, 0);
   const ones = average(info.pulses, 1);
-  const calc = 10000 / (zero + ones);
+  const calc = 10000 / ones;
   logger.info('reset counter: ', zero, ones, calc);
 });
 
